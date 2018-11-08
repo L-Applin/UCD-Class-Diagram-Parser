fd526cbc354aa8129aa9c84c56bce24feb9017cb
payne911 <jeremi.grenier.berthiaume@gmail.com>
2018-11-07 21:54:54 -0500

    eclipse sucks

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,12 +1,12 @@
 package app;
 
-import com.oracle.tools.packager.Platform;
+//import com.oracle.tools.packager.Platform;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import screenDisplay.ScreenController;
 
 import java.io.IOException;
-import static app.Utils.Log;
+//import static app.Utils.Log;
 
 public class Main extends Application {
 
@@ -23,15 +23,15 @@ public class Main extends Application {
      */
     public static final boolean CREATE_SELF_UCD = false;
 
-    public static final Platform os = Platform.getPlatform();
+    //public static final Platform os = Platform.getPlatform();
     public static final String USER_PATH = System.getProperty("user.dir");
 
 
     @Override
     public void start(Stage primaryStage) throws IOException {
 
-        Log.log(String.format("Running of system : %s", os.name()));
-        Log.log(String.format("Project root path : %s", USER_PATH));
+        //Log.log(String.format("Running of system : %s", os.name()));
+        //Log.log(String.format("Project root path : %s", USER_PATH));
 
         final ScreenController screen = new ScreenController(primaryStage, getParameters());
         screen.setupMainDisplay();
